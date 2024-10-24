b00fb55a2d178c9a5544011642add0c9095ee792
Wojtazz <marcin.witas72@gmail.com>
2024-10-24 22:56:55 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 	// const env = loadEnv(mode, process.cwd());
 
 	return {
